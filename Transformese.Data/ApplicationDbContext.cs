9abf19230ce5c9689ca65df9506bcad0f87d60ee
Milone <milonegabriel000@gmail.com>
2025-12-05 05:45:06 -0300

    chore: adiciona config e seed data

--- Transformese.Data/ApplicationDbContext.cs
+++ Transformese.Data/ApplicationDbContext.cs
@@ -29,6 +29,10 @@ namespace Transformese.Data
                 .IsRequired()
                 .HasMaxLength(14);
 
+            modelBuilder.Entity<Candidato>()
+                .Property(c => c.RendaFamiliar)
+                .HasColumnType("decimal(18,2)");
+
             modelBuilder.Entity<Funcionario>().HasData(new Funcionario
             {
                 Id = 1,
